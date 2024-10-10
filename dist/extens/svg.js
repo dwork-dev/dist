582708e3c88060e38e3b53c37345d081de90d483
dipgle <qvt0484@gmail.com>
2024-10-10 17:38:11 +0700

    Update svg.js

--- dist/extens/svg.js
+++ dist/extens/svg.js
@@ -1,1 +1,46 @@
-(()=>{var v={height:18,width:18,viewBox:"0 -960 960 960",fill:"#9c9c9c"};function l(n){let{svg:r,path:o,title:i}=$tags("http://www.w3.org/2000/svg");n.innerText="";var s=n.options(),t=$data.find(e=>e.name==s.name),a={...v};[...Object.keys(v),"name"].map(e=>{if(typeof s[e]<"u")return a[e]=s[e];if(t&&typeof t[e]<"u")return a[e]=t[e]}),n.$el=t?r(a,o({d:t?.svg})):null,console.log("create",n),$add(n,n.$el)}$el("t-svg",[],n=>{l(n)},()=>{name=="t-options"&&valOld&&l(el)});$el("t-search",[],n=>{var r=n.options(),o=r.icon||{};n.innerText="";var i="";o.name&&(i=`<t-svg t-options="name:'${o.name}',fill:'${o.fill}'"></t-svg>`),console.log(1122,n),console.log(1122,typeof n.innerHTML,i),n.innerHTML=i},(n,r,o,i)=>{if(r=="t-options"&&i){var s=n.options(),t=s.icon||{};n.innerText="";var a="";t.name&&(a=`<t-svg t-options="name:'${t.name}',fill:'${t.fill}'"></t-svg>`),n.innerHTML=a}});})();
+    var ops = {
+        height: 24,
+        width: 24,
+        viewBox: "0 -960 960 960",
+        fill: "var(--c-dgreen)"
+    }
+    function render(el) {
+        let { svg, path, title } = $tags("http://www.w3.org/2000/svg");
+        el.innerText = "";
+        var o = el.options();
+        var s = $data.find(f => f.name == o.name);
+        let arrPathTag = [];
+        console.log('s', s)
+        if (Array.isArray(s?.svg)) {
+            for (let item of s.svg) {
+                arrPathTag.push(path({ d: item??''}))
+            }
+        } else {
+            arrPathTag.push(path({ d: s?.svg }))
+        }
+        var ob = { ...ops };
+        [...Object.keys(ops), "name"].map(k => {
+            if (typeof o[k] != "undefined") {
+                return ob[k] = o[k];
+            }
+            if (s && typeof s[k] != "undefined") {
+                return ob[k] = s[k];
+            }
+        })
+        el.$el = s ? svg(
+            ob,
+            ...arrPathTag
+        ) : null
+        $add(el, el.$el);
+    }
+    $el("x-svg", [], (el) => {
+        render(el);
+        el.changeName = (n) => {
+            let d = $data.find(item => item.name === n)
+            el.querySelector('path')?.setAttribute('d', d?.svg);
+        }
+    }, (el, name, valNew, valOld) => {
+        if (name == "t-options" && valOld) {
+            render(el);
+        }
+    })
