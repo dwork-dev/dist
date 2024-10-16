cdd3e736718eacaa11a40f48cc0702f79bcdd8a5
dipgle <qvt0484@gmail.com>
2024-10-16 18:16:32 +0700

    Update date.js
    update av $$colors

--- dist/extens/date.js
+++ dist/extens/date.js
@@ -1,4 +1,12 @@
-(()=>{$el("t-date",[],t=>{v(t)},(t,i,n,s)=>{});function v(t){var i=t.options();t.innerHTML=`<div class="t-datebox df aic"><div class="posR">
+(async()=>{
+  $el("t-date",[],(el)=>{
+  init(el);
+},(el,name,newVal,oldVal)=>{
+  
+});
+function init(el){
+  var op=el.options();
+  el.innerHTML=`<div class="t-datebox df aic"><div class="posR">
   <span class="bd bda5px p3px df aic">
   	<span class="t-input-date bd0px posr">
      	<span class="">dd</span>
@@ -12,7 +20,7 @@
      	<span class="">yyyy</span>
           <input class="tac bd0px posa w100% h100% t0px l0px" placeholder="yyyy">
      </span>
-     <t-svg class="cicon w23px h23px ml5px cr" t-options="name:'user',fill:'${$colors.icon||"gray"}'"></t-svg>
+     <t-svg class="cicon w23px h23px ml5px cr" t-options="name:'user',fill:'${$$colors.bcolor||'gray'}'"></t-svg>
   </span>
      <span class="t-select dn">
      	<div class="posA w{auto} h{auto}">
@@ -26,4 +34,127 @@
             </div>
        </div>
      </span>
-  </div></div>`,t.onchange=()=>{console.log("onchange",t.val())},t.querySelector(".btn_today").onclick=()=>{t.val(Date.now())},t.querySelector(".btn_clear").onclick=()=>{n.forEach(s=>{s.value=""}),t.querySelector(".t-select").classList.add("dn"),typeof t.onchange=="function"&&t.onchange(t)},t.querySelector(".cicon").onclick=()=>{var s=t.val(),e=new Date(s||new Date),a=e.getDate(),l=e.getMonth(),g=1;e=new Date(`${e.getFullYear()}-${e.getMonth()+1}-1`);var o=1;for(t.querySelector(".t-select .d-body").innerHTML="";e.getDay()>o;){var p=new Date(e.getTime()-(e.getDay()-o)*864e5);r(p,1),o++}for(;e.getMonth()==l;)r(e),e=new Date(e.getTime()+864e5);for(o=e.getDay();o<=7;)r(e,1),o++,e=new Date(e.getTime()+864e5);t.querySelector(".t-select").classList.remove("dn");function r(c,u){var d=document.createElement("span");d.innerHTML=`<div class="w23px h23px df aic cr jcC ${c.getDate()==a?"bd":""} ${c.getMonth()==l&&c.getDate()==a?"bg{green}":""} ${u?"bg#ccc c#9e9e9e":""}">${c.getDate()}</div>`,t.querySelector(".t-select .d-body").append(d),d.onclick=()=>{t.val(c)}}};var n=t.querySelectorAll("input");n.forEach((s,e)=>{s.addEventListener("keydown",a=>{a.keyCode==39?e<2&&s.selectionEnd==2&&setTimeout(()=>{n[e+1].focus()}):a.keyCode==37?e>0&&s.selectionEnd==2&&setTimeout(()=>{n[e-1].focus()}):a.keyCode==38||a.keyCode==40,console.log(a)}),s.addEventListener("keypress",a=>{if(a.key==13)return t.onchange(t);if("0,1,2,3,4,5,6,7,8,9".includes(a.key)){if(e<2&&a.target.value.length==2||e==2&&a.target.value.length==4)return event.preventDefault();if(e<2&&a.target.value.length==1){var l=e+1;l<3&&setTimeout(()=>{n[l].focus()})}else e==2&&a.target.value.length==3}else return event.preventDefault();s.previousElementSibling.innerText=s.value.padStart(e<2?2:4,0)})}),n[1].addEventListener("blur",s=>{var e=+n[1].value||new Date().getMonth()+1;e>12&&(e=12),n[1].value=(e+"").padStart(2,0)}),t.val=s=>{if(s){var e=new Date(s);n[0].value=e.getDate(),n[1].value=e.getMonth()+1,n[2].value=e.getFullYear()}var e=[n[2].value,n[1].value,n[0].value].join("-"),a=new Date(e);if(a+""!="Invalid Date")return t.querySelector(".t-select").classList.add("dn"),[a.getFullYear(),a.getMonth()+1,a.getDate()].join("-")}}})();
+  </div></div>`;
+  el.onchange=()=>{
+    console.log("onchange",el.val())
+  }
+  el.querySelector(".btn_today").onclick=()=>{
+    el.val(Date.now());
+  }
+  el.querySelector(".btn_clear").onclick=()=>{
+    ips.forEach(ip=>{
+      ip.value=``;
+    });
+    el.querySelector(".t-select").classList.add("dn");
+    typeof el.onchange=="function"&&el.onchange(el);
+  }
+  el.querySelector(".cicon").onclick=()=>{
+    var v=el.val();
+    var day=new Date(v||new Date);
+    var date=day.getDate();
+    var month=day.getMonth();
+    var di=1;
+    day=new Date(`${day.getFullYear()}-${day.getMonth()+1}-1`);
+    var i=1;
+    el.querySelector(".t-select .d-body").innerHTML=``;
+    while(day.getDay()>i){
+      var t=new Date(day.getTime()-(day.getDay()-i)*86400000);
+      //days.push(t);
+      pushD(t,1)
+      i++;
+    }
+    while(day.getMonth()==month){
+      //days.push(day);
+      pushD(day);
+      day=new Date(day.getTime()+86400000);
+    }
+    i=day.getDay();
+    while(i<=7){
+      pushD(day,1);
+      i++;
+      day=new Date(day.getTime()+86400000);
+    }
+    el.querySelector(".t-select").classList.remove("dn");
+    function pushD(day,outM){
+      var s=document.createElement("span");
+      s.innerHTML=`<div class="w23px h23px df aic cr jcC ${day.getDate()==date?'bd':''} ${day.getMonth()==month&&day.getDate()==date?'bg{green}':''} ${outM?'bg#ccc c#9e9e9e':''}">${day.getDate()}</div>`;
+      el.querySelector(".t-select .d-body").append(s);
+      s.onclick=()=>{
+        el.val(day);
+      }
+    }
+  }
+  var ips=el.querySelectorAll("input");
+  ips.forEach((ip,i)=>{
+    ip.addEventListener("keydown",evt=>{
+      //event.preventDefault();
+      if(evt.keyCode==39){ /* arrow right*/
+        if(i<2 && ip.selectionEnd==2){
+          setTimeout(()=>{ips[i+1].focus()})
+        }
+      }else if(evt.keyCode==37){ /* arrow left*/
+        if(i>0 && ip.selectionEnd==2){
+          setTimeout(()=>{ips[i-1].focus()})
+        }
+      }else if(evt.keyCode==38){ /* arrow up */
+        
+      }else if(evt.keyCode==40){	/* arrow down */
+        
+      }
+      console.log(evt);
+    });
+    ip.addEventListener("keypress",evt=>{
+      //console.log("0,1,2,3,4,5,6,7,8,9".includes(evt.key))
+      if(evt.key==13){
+        return el.onchange(el);
+      }
+      if(!"0,1,2,3,4,5,6,7,8,9".includes(evt.key)){
+         return event.preventDefault();
+      }else if(i<2 && evt.target.value.length==2||i==2&&evt.target.value.length==4){
+        //console.log("evt.target.value",evt.target.value)
+         return event.preventDefault();
+      }else if(i<2 && evt.target.value.length==1){
+        var ni=i+1;
+        if(ni<3){
+          setTimeout(()=>{
+            ips[ni].focus();
+          })
+        } 
+      }else if(i==2&&evt.target.value.length==3){
+        
+      }
+      ip.previousElementSibling.innerText=
+        ip.value.padStart(i<2?2:4,0);
+      //console.log(evt,evt.target.value)
+    });
+  });
+  ips[1].addEventListener("blur",evt=>{
+    var v=+ips[1].value||new Date().getMonth()+1;
+    if(v>12){
+      v=12;
+    }
+    ips[1].value=(v+"").padStart(2,0);
+  })
+  el.val=(v)=>{
+    if(v){
+      var d=new Date(v);
+      ips[0].value=d.getDate();
+      ips[1].value=d.getMonth()+1;
+      ips[2].value=d.getFullYear();
+    }
+    var d=[ips[2].value,ips[1].value,ips[0].value].join("-");
+    var t=new Date(d);
+    if(t+""=='Invalid Date'){
+      return
+    }
+    el.querySelector(".t-select").classList.add("dn");
+    return [t.getFullYear(),t.getMonth()+1,t.getDate()].join('-');
+  }
+}
+function render(el){
+  
+}
+function setData(d){
+  
+}
+})();
