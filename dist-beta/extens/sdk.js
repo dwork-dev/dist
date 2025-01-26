399d77e5c7ff3df4a44fa57b69fd6f037031fcba
dipgle <qvt0484@gmail.com>
2025-01-26 16:11:46 +0700

    Update sdk.js

--- dist-beta/extens/sdk.js
+++ dist-beta/extens/sdk.js
@@ -1,4 +1,10 @@
-
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+Element.prototype.el=function(id){
+  return this.querySelector(id);
+}
+Element.prototype.els=function(id){
+  return this.querySelectorAll(id);
+};
 ((dk)=>{
   var _user;
   var _domain=location.host.split(".").slice(-2).join(".");
@@ -785,13 +791,14 @@
         });
       }
       self.loginF=(cb)=>{
-        $dlg.form({
-          title:'Đăng nhập',
-          template:`<t-login><t-login>`,
-          width: '400',
-          onshown: frm=>{
-            if(_user && _user.token){
-              frm.getBody().innerHTML=`<div class="">
+        if(_user && _user.token){
+          $dlg.form({
+            title:'Đăng nhập',
+            url: `/rcpanel/user/login.html`,
+            width: '400',
+            onshown: frm=>{
+              if(_user && _user.token){
+                frm.getBody().innerHTML=`<div class="">
             <div class="dF">
             <div class=""></div>
             <div class="">${_user.fullname||_user.username}</div>
@@ -800,16 +807,16 @@
             	<x-button t-options="name:'btn_signout',title:'Logout'"></x-button>
             </div>
             </div>`;
-              frm.el('[name="btn_signout"]').onclick=()=>{
-                self.logout(rs=>{
-                  if(rs.status_code==200){
-                    location.reload();
-                  }
-                  $dlg.error(rs)
-                });
-              }
-            }else{
-              frm.getBody().innerHTML=`<div class="">
+                frm.el('[name="btn_signout"]').onclick=()=>{
+                  self.logout(rs=>{
+                    if(rs.status_code==200){
+                      location.reload();
+                    }
+                    $dlg.error(rs)
+                  });
+                }
+              }else{
+                frm.getBody().innerHTML=`<div class="">
     	<div class="dG gtc{80px;auto} aic p5px">
      	<div class="">
      		Tài khoản
@@ -833,24 +840,50 @@
      		<x-button t-options="title:'Đăng nhập & tải lại',class:['p{5px;10px}','bd','cr','btn_sendr']"></x-button>
      	</div>
      </div>`;
-              setTimeout(()=>{
-                frm.els(".btn_send,.btn_sendr").forEach(el=>{
-                  el.onclick=()=>{
-                    self.login(frm.el('[name="username"]').value,frm.el('[name="password"]').value,"",rs=>{
-                      if(rs.status_code == 200){
-                        frm.close();
-                        _user={
-                          token: rs.data[_token],
-                          ...rs.data.user
-                        };
-                        //location.reload();
-                      }
-                      $dlg.error(rs);
-                    });
-                  }
-                })
-              },1000);
+                setTimeout(()=>{
+                  frm.els(".btn_send,.btn_sendr").forEach(el=>{
+                    el.onclick=()=>{
+                      self.login(frm.el('[name="username"]').value,frm.el('[name="password"]').value,"",rs=>{
+                        if(rs.status_code == 200){
+                          frm.close();
+                          _user={
+                            token: rs.data[_token],
+                            ...rs.data.user
+                          };
+                          //location.reload();
+                        }
+                        $dlg.error(rs);
+                      });
+                    }
+                  })
+                },1000);
+              }
             }
+          });
+          return;
+        }
+        $dlg.form({
+          title:'Đăng nhập',
+          url: `/rcpanel/user/login-form.html`,
+          width: '400',
+          onshown: frm=>{
+            console.log(frm);
+            frm.els(".btn_send,.btn_sendr").forEach(el=>{
+              el.onclick=()=>{
+                var d=frm.get();
+                self.login(d.username,d.password,"",rs=>{
+                  if(rs.status_code == 200){
+                    frm.close();
+                    _user={
+                      token: rs.data[_token],
+                      ...rs.data.user
+                    };
+                    //location.reload();
+                  }
+                  $dlg.error(rs);
+                });
+              }
+            })
           }
         });
       }
@@ -881,4 +914,3 @@ function dataURItoBlob(dataURI) {
   //New Code
   return new Blob([ab], {type: mimeString});
 }
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
